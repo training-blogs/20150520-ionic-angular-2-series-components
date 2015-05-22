65a3df5ee8ae2e188a21c203ac35b5eab1355b4e
Alexis THOMAS <fr.alexisthomas@gmail.com>
2015-05-22 23:54:53 +0200

    Add my-component

---
+++ component.js
@@ -0,0 +1,16 @@
+import {Component, Template} from 'angular2/angular2'
+
+@Component({
+    selector: 'my-component'
+})
+@View({
+    inline: "<div>Hello my name is {{name}}</div>"
+})
+export class MyComponent {
+    constructor() {
+        this.name = 'Max'
+    }
+    sayMyName() {
+        console.log('My name is', this.name)
+    }
+}
